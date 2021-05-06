dc745f108d0ea590f4b22828ba16c7f90e76817a
ramxis <rameez.saleem@gmail.com>
2021-05-06 20:34:00 +0200

    added sendTransaction

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,6 @@
 var web3;
+var Tx = require('ethereumjs-tx').Transaction;
+var keythereum = require("keythereum");
 $(document).ready(function() {
     console.log("script loaded");
     if (typeof  window.ethereum !== 'undefined') {
@@ -13,12 +15,17 @@ $(document).ready(function() {
 
 
 async function signTransaction() {
-    console.log("inside signing transactions")
+    console.log("inside signing transactions");
+    var privateKey = Buffer.from('2ff716e2adfb575da83db6191bccb4506237050fd924312b80d3665c9ed95741', 'hex');
+
     const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
     const account = accounts[0];
     var _to = $('#toAddress').val();
     var _value = web3.utils.toWei($('#amount').val(), "ether");
-    console.log(_value);
+
+
+    //
+    web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
     var tx = {
         from: account,
         value: _value,
@@ -27,6 +34,14 @@ async function signTransaction() {
     }
     var signedTx = await web3.eth.sendTransaction(tx);
     console.log(signedTx);
+    // create a raw tx
+    var tx = new Tx(tx);
+    tx.sign(privateKey);
+
+    // var serializedTx = tx.serialize();
+    // console.log('serialzied');
+    // var receipt = await web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'));
+    // console.log(receipt);
     // // console.log("signing transactions....")
 
     // const signedTx = await _web3Provider.eth.signTransaction(tx);
